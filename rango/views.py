4d13ff2184c10658d2485c178b4f9083ca16bda8
Yee Hou Teoh <2471020t@student.gla.ac.uk>
2020-02-13 01:31:28 +0000

    chapter 3 exercise

--- rango/views.py
+++ rango/views.py
@@ -6,6 +6,9 @@ from django.http import HttpResponse
 def index(request):
     """ view 1: index
 
+        Note:
+            -\n doesnt work?
+
         Args:
             param1 (HttpRequest object): lives in the django.http module.
             named request due to convention.
@@ -13,8 +16,17 @@ def index(request):
         Returns:
             A HttpResponse object. Takes a string parameter representing the
             content of the page we wish to send to the client requesting the
-            view
+            view.
+            Shows text with hyperlink to about
 
 
     """
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse('<a href="/rango/about/">About</a>\nRango says hey there partner!')
+
+def about(request):
+    """ view 2: about
+
+        Returns:
+            A HttpResponse object to show text with hyperlink back to index.
+    """
+    return HttpResponse('<a href="/rango/">Index</a>\nRango says here is the about page')
